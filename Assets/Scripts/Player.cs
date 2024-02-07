c1fddd590c6835bf6b5dd62101944f8a10828916
SJDavies2020 <stevenjdavies2013@gmail.com>
2024-02-07 11:37:27 -0500

    Base Class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,60 +6,39 @@ public class Player : MonoBehaviour
 {
     // Animator component reference
     private Animator animator;
-
     // Input variables
     private float horizontalInput;
     private float verticalInput;
-
     // Speed and rotation variables
     public float moveSpeed = 5.0f;
     public float rotationSpeed = 120.0f;
-
     Vector2 moveMent;
     Rigidbody2D rb;
-
     private void Start()
     {
         // Get the Animator component attached to the player
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
-
     private void Update()
     {
         // Get player input
         moveMent.x = Input.GetAxisRaw("Horizontal");
-
-        Debug.Log("X: " + moveMent.x);
+        // Debug.Log("X: " + moveMent.x);
         moveMent.y = Input.GetAxisRaw("Vertical");
-
-        Debug.Log("Y: " + moveMent.y);
+        // Debug.Log("Y: " + moveMent.y);
         // Set animator parameters based on input
-
         animator.SetFloat("MoveY", moveMent.x);
-
         animator.SetFloat("MoveX", moveMent.y);
-
         // Move the player
         Move();
         // Rotate();
     }
-
     private void Move()
     {
         Vector2 movement = new Vector2(moveMent.x, moveMent.y).normalized * moveSpeed * Time.deltaTime;
         // Apply movement
         transform.Translate(movement);
-        Debug.Log("Movement: " + moveMent);
-
+        //  Debug.Log("Movement: " + moveMent);
     }
-
-    //private void Rotate()
-    //{
-    //    // Calculate rotation amount based on input
-    //    float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
-
-    //    // Apply rotation
-    //    transform.Rotate(0, rotation, 0);
-    //}
 }
