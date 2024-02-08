1c1bd0e2c2b03dcafdad5c93de0cad05c10479f9
JoeyBamboozles <joemcphee777@hotmail.com>
2024-02-07 21:18:51 -0500

    Gorilla animations and GorillaController Impemented

---
+++ Assets/Scripts/GorillaController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GorillaController : MonoBehaviour
+{
+
+    private Transform target;
+    public float speed;
+    private Vector2 movement;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = transform.position.x;
+        movement.y = transform.position.y;
+
+        animator.SetFloat("MoveY", movement.x);
+        animator.SetFloat("MoveX", movement.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+    }
+}
