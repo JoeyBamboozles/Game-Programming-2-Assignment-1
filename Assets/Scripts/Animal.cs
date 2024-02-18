5b238faf3331c6ee28a8d03a57d9e06dc949cfda
SJDavies2020 <stevenjdavies2013@gmail.com>
2024-02-18 11:03:51 -0500

    Updated Animal Script

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -3,33 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class Animal : MonoBehaviour
 {
-    public class Animal
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Species { get; set; }
+
+    // Constructor
+    public Animal(string name, int age, string species)
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Species { get; set; }
+        Name = name;
+        Age = age;
+        Species = species;
+    }
 
-        // Constructor
-        public Animal(string name, int age, string species)
-        {
-            Name = name;
-            Age = age;
-            Species = species;
-        }
+    // Method to make the animal make a sound
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("The animal makes a sound.");
+    }
 
-        // Method to make the animal make a sound
-        public virtual void MakeSound()
-        {
-            Console.WriteLine("The animal makes a sound.");
-        }
-
-        // Method to display information about the animal
-        public virtual void Display()
-        {
-            Console.WriteLine($"Name: {Name}, Age: {Age}, Species: {Species}");
-        }
+    // Method to display information about the animal
+    public virtual void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Species: {Species}");
     }
 }
+
 
--- Assets/Scripts/HorseController.cs
+++ Assets/Scripts/HorseController.cs
@@ -3,76 +3,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Scripts
-{
     #region RegionName
-    public class Horse : Animal
+public class Horse : Animal
+{
+    public Horse(string name, int age, string species) : base(name, age, species)
     {
-        public Horse(string name, int age, string species) : base(name, age, species)
-        {
-            Name = name;
-            Age = age;
-            Species = species;
-        }
-        public override void MakeSound()
-        {
-            Debug.Log("The horse neighs: ");
-        }
-        //public override void Display()
-        //{
-        //    Debug.Log($"Name: {Name}, Age: {Age}, Species: Horse");
-        //}
+        Name = name;
+        Age = age;
+        Species = species;
     }
-    #endregion
-    public class HorseController : MonoBehaviour
+    public override void MakeSound()
     {
-        private Transform target;
-        public float speed;
-        private Vector2 movement;
-        private Animator animator;
-        Horse myHorse;
-        private bool isInProximity = false;
-        // Start is called before the first frame update
-        void Start()
+        Debug.Log("The horse neighs: ");
+    }
+    //public override void Display()
+    //{
+    //    Debug.Log($"Name: {Name}, Age: {Age}, Species: Horse");
+    //}
+    }
+#endregion
+public class HorseController : MonoBehaviour
+{
+    private Transform target;
+    public float speed;
+    private Vector2 movement;
+    private Animator animator;
+    Horse myHorse;
+    private bool isInProximity = false;
+    public Transform[] patrolPoints;
+    public int targetPoint;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        myHorse = new Horse("Bob", 13, "Equine");
+    }
+
+    void Update()
+    {
+        Vector3 direction = (patrolPoints[targetPoint].position - transform.position).normalized;
+
+        if (transform.position == patrolPoints[targetPoint].position)
         {
-            target = GameObject.Find("Player").GetComponent<Transform>();
-            animator = GetComponent<Animator>();
-            myHorse = new Horse("Bob", 13, "Equine");
+            increaseTargetInt();
         }
-        // Update is called once per frame
-        void Update()
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+
+        // Update animator parameters based on movement direction
+        animator.SetFloat("MoveX", direction.x);
+        animator.SetFloat("MoveY", direction.y);
+    }
+    void increaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
         {
-            if (isInProximity == true)
-            {
-                movement.x = transform.position.x;
-                movement.y = transform.position.y;
-                animator.SetFloat("MoveY", movement.x);
-                animator.SetFloat("MoveX", movement.y);
-                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-            }
-            else
-            {
-                animator.SetFloat("MoveY", 0);
-                animator.SetFloat("MoveX", 0);
-            }
-        }
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
-                myHorse.MakeSound();
-                myHorse.Display();
-                isInProximity = true;
-            }
-        }
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
-                myHorse.MakeSound();
-                isInProximity = false;
-                myHorse.Display();
-            }
+            targetPoint = 0;
         }
     }
-}
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+} 
+
+
+
+
+        
+
+
+       
+
+
+
