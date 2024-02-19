23b255749c960e527d1a05c5e521e11d4c5236b2
JoeyBamboozles <joemcphee777@hotmail.com>
2024-02-18 21:51:38 -0500

    Fixed Audio Triggers

--- Assets/Scripts/GorillaController.cs
+++ Assets/Scripts/GorillaController.cs
@@ -3,45 +3,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GorillaController : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] public Transform[] patrolPoints;
-    [SerializeField] public int targetPoint;
-    [SerializeField] public float speed;
-     private Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Gorilla : Animal 
     {
-        targetPoint = 0;
-        animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        Vector3 direction = (patrolPoints[targetPoint].position - transform.position).normalized;
-
-        if (transform.position == patrolPoints[targetPoint].position)
+        public Gorilla(string name, int age, string species) : base(name, age, species)
         {
-            increaseTargetInt();
+            Name = name;
+            Age = age;
+            Species = species;
         }
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
-
-        // Update animator parameters based on movement direction
-        animator.SetFloat("Horizontal", direction.x);
-        animator.SetFloat("Vertical", direction.y);
-
-
+        public override void MakeSound()
+        {
+            Debug.Log("Grumblebuns packs a vicious roar!"); 
+        }
     }
 
-    void increaseTargetInt()
+    public class GorillaController : MonoBehaviour 
     {
-        targetPoint++;
-        if (targetPoint >= patrolPoints.Length)
+        public Transform[] patrolPoints;
+        public int targetPoint;
+        public float speed;
+        private Animator animator;
+        Gorilla myGorilla; 
+
+       
+        void Start()
         {
             targetPoint = 0;
+            animator = GetComponent<Animator>();
+            myGorilla = new Gorilla("Grumblebuns", 20, "Gorilla"); // Changed Lion's info to Gorilla's info
+        }
+
+        
+        void Update()
+        {
+            Vector3 direction = (patrolPoints[targetPoint].position - transform.position).normalized;
+
+            if (transform.position == patrolPoints[targetPoint].position)
+            {
+                increaseTargetInt();
+            }
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+
+            
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+        }
+
+        void increaseTargetInt()
+        {
+            targetPoint++;
+            if (targetPoint >= patrolPoints.Length)
+            {
+                targetPoint = 0;
+            }
         }
     }
 }
