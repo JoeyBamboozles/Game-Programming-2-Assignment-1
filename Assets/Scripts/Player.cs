daf641ee8e5076ff1d6f8ba14dbd11f7eeb07ed2
JoeyBamboozles <joemcphee777@hotmail.com>
2024-02-20 19:37:50 -0500

    Added interaction text

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using TMPro; // Add this line to use TextMeshPro
 
 public class Player : MonoBehaviour
 {
@@ -22,79 +23,112 @@ public class Player : MonoBehaviour
     public AudioSource thePig;
     public string theAnimal;
 
+    public TextMeshProUGUI gorillaText;
+    public TextMeshProUGUI lionText;
+    public TextMeshProUGUI horseText;
+    public TextMeshProUGUI pigText;
+
+    private bool isFKeyPressed = false;
+
     private void Start()
     {
         // Get the Animator component attached to the player
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        // Make the TextMeshPro UIs invisible at the start
+        gorillaText.gameObject.SetActive(false);
+        lionText.gameObject.SetActive(false);
+        horseText.gameObject.SetActive(false);
+        pigText.gameObject.SetActive(false);
     }
+
     private void Update()
     {
         // Get player input
         moveMent.x = Input.GetAxisRaw("Horizontal");
-        // Debug.Log("X: " + moveMent.x);
         moveMent.y = Input.GetAxisRaw("Vertical");
-        // Debug.Log("Y: " + moveMent.y);
         // Set animator parameters based on input
         animator.SetFloat("MoveY", moveMent.x);
         animator.SetFloat("MoveX", moveMent.y);
         // Move the player
         Move();
-        // Rotate();
 
-        if (theAnimal == "Gorilla" && Input.GetKeyDown(KeyCode.F))
+        // Check if F key is pressed
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            theGorilla.Play();
+            isFKeyPressed = true;
+            PlayAnimalSound();
         }
-        else if (theAnimal == "Lion" && Input.GetKeyDown(KeyCode.F))
-        {
-            theLion.Play();
-        }
-        else if (theAnimal == "Horse" && Input.GetKeyDown(KeyCode.F))
-        {
-            theHorse.Play();
-        }
-        else if (theAnimal == "Pig" && Input.GetKeyDown(KeyCode.F))
-        {
-            thePig.Play();
-        }
-
     }
+
+    private void PlayAnimalSound()
+    {
+        // Play animal sound based on theAnimal
+        switch (theAnimal)
+        {
+            case "Gorilla":
+                theGorilla.Play();
+                gorillaText.gameObject.SetActive(false); // Hide text after F key is pressed
+                break;
+            case "Lion":
+                theLion.Play();
+                lionText.gameObject.SetActive(false); // Hide text after F key is pressed
+                break;
+            case "Horse":
+                theHorse.Play();
+                horseText.gameObject.SetActive(false); // Hide text after F key is pressed
+                break;
+            case "Pig":
+                thePig.Play();
+                pigText.gameObject.SetActive(false); // Hide text after F key is pressed
+                break;
+        }
+    }
+
     private void Move()
     {
         Vector2 movement = new Vector2(moveMent.x, moveMent.y).normalized * moveSpeed * Time.deltaTime;
         // Apply movement
         transform.Translate(movement);
-        //  Debug.Log("Movement: " + moveMent);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Gorilla")
+        // Set theAnimal based on collision
+        if (!isFKeyPressed)
         {
-            Debug.Log("Colliding with Gorilla");
-            theAnimal = "Gorilla";
-        }
-        else if (collision.gameObject.tag == "Horse")
-        {
-            Debug.Log("Colliding with Horse");
-            theAnimal = "Horse";
-        }
-        else if (collision.gameObject.tag == "Lion")
-        {
-            Debug.Log("Colliding with Lion");
-            theAnimal = "Lion";
-        }
-        else if (collision.gameObject.tag == "Pig")
-        {
-            Debug.Log("Colliding with Pig");
-            theAnimal = "Pig";
+            switch (collision.gameObject.tag)
+            {
+                case "Gorilla":
+                    theAnimal = "Gorilla";
+                    gorillaText.gameObject.SetActive(true); // Show text when colliding with Gorilla
+                    break;
+                case "Lion":
+                    theAnimal = "Lion";
+                    lionText.gameObject.SetActive(true); // Show text when colliding with Lion
+                    break;
+                case "Horse":
+                    theAnimal = "Horse";
+                    horseText.gameObject.SetActive(true); // Show text when colliding with Horse
+                    break;
+                case "Pig":
+                    theAnimal = "Pig";
+                    pigText.gameObject.SetActive(true); // Show text when colliding with Pig
+                    break;
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
+        // Reset isFKeyPressed when exiting the trigger
+        isFKeyPressed = false;
 
+        // Make all TextMeshPro UIs invisible when not colliding
+        gorillaText.gameObject.SetActive(false);
+        lionText.gameObject.SetActive(false);
+        horseText.gameObject.SetActive(false);
+        pigText.gameObject.SetActive(false);
+    }
 }
